bcdfa1da4897bafe7023eba60dbc51fe93180d92
hilmanfajrian <hilmanfajrian@gmail.com>
2026-05-02 03:47:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,13 @@ chown -R openclaw:openclaw /data
 chmod 700 /data
 
 if [ ! -d /data/.linuxbrew ]; then
-  cp -a /home/linuxbrew/.linuxbrew /data/.linuxbrew
+ cp -a /home/linuxbrew/.linuxbrew /data/.linuxbrew
 fi
 
 rm -rf /home/linuxbrew/.linuxbrew
 ln -sfn /data/.linuxbrew /home/linuxbrew/.linuxbrew
+
+# Install WA plugin on every startup
+gosu openclaw openclaw plugins install @openclaw/whatsapp || true
 
 exec gosu openclaw node src/server.js
